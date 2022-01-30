ee77f11a3e83ee65aef00d9c30385038812a117b
r-40021 <75155258+r-40021@users.noreply.github.com>
2022-01-30 14:12:41 +0900

    [Add]Display out

--- script/content.js
+++ script/content.js
@@ -1,6 +1,6 @@
 const elements = document.querySelectorAll('input:not([type]),input[type=text],input[type=search],input[type=tel],input[type=url],input[type=email],input[type=password],input[type=number],textarea');
 let countDisplay; // 文字数ディスプレイの要素
-let opacityTimeout; // 2秒間操作がなかったら透明度を下げる timeout
+let opacityTimeout, displayTimeout; // 2秒間操作がなかったら透明度を下げる timeout と、 10秒間操作がなかったら非表示にする timeout
 
 elements.forEach((element) => {
   element.addEventListener('input', () => {
@@ -31,7 +31,7 @@ elements.forEach((element) => {
  * @param {Element} element 対象とするテキストボックスの要素 
  */
 function createDisplayElement(element) {
-  clearTimeout(opacityTimeout)
+  setOpacityTimeout(); // 2秒間操作がなかったら透明度を下げる
   if (!countDisplay) {
     // 文字数を表示する要素を生成
     countDisplay = document.createElement('div');
@@ -45,24 +45,31 @@ function createDisplayElement(element) {
     countDisplay.style.fontSize = '18px';
     countDisplay.style.zIndex = 999999999999999999;
     countDisplay.style.fontFamily = '"Helvetica Neue", "Segoe UI", Roboto';
-    if(!window.matchMedia('(prefers-reduced-motion: reduce)').matches) countDisplay.style.transition = 'opacity .2s, top .2s ease-out, left .2s ease-out, right .2s ease-out'
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) countDisplay.style.transition = 'opacity .2s, top .2s ease-out, left .2s ease-out, right .2s ease-out'
 
     countDisplay.textContent = element.value.length;
-    
+
     setCoordinate(element);
 
     document.body.appendChild(countDisplay);
   }
-  countDisplay.style.opacity = 0.9; // 透明度をもとに戻す
-  setOpacityTimeout(); // 2秒間操作がなかったら透明度を下げる
 }
 
 /**
  * 文字数ディスプレイの透明度を2秒後に下げる
  */
 function setOpacityTimeout() {
+  clearTimeout(opacityTimeout);
+  clearTimeout(displayTimeout);
+  if (countDisplay) {
+    countDisplay.style.opacity = 0.9
+    countDisplay.style.display = '';
+  }
   opacityTimeout = setTimeout(() => {
-    if(countDisplay) countDisplay.style.opacity = 0.12;
+    if (countDisplay) countDisplay.style.opacity = 0.2;
+    displayTimeout = setTimeout(() => {
+      if (countDisplay) countDisplay.style.display = 'none';
+    }, 8000);
   }, 2000);
 }
 
@@ -73,18 +80,18 @@ function setOpacityTimeout() {
  */
 function setCoordinate(element) {
   const clientRect = element.getBoundingClientRect();
-    let x;
-    if (clientRect.right + window.scrollX + 5 + 10 * element.value.length.toString().length + 10 * 2 <= window.scrollX + window.innerWidth - 10) {
-      x = clientRect.right + window.scrollX + 5 + 'px';
-      countDisplay.style.right = '';
-      countDisplay.style.left = x;
-    } else {
-      x = window.innerWidth - (window.scrollX + window.innerWidth - 10) + 'px';
-      countDisplay.style.left = '';
-      countDisplay.style.right = x;
-    }
-    const y = Math.min(clientRect.top + element.clientHeight + window.scrollY + 5, window.scrollY + window.innerHeight - 40 ) + 'px';
-    countDisplay.style.top = y;
+  let x;
+  if (clientRect.right + window.scrollX + 5 + 10 * element.value.length.toString().length + 10 * 2 <= window.scrollX + window.innerWidth - 10) {
+    x = clientRect.right + window.scrollX + 5 + 'px';
+    countDisplay.style.right = '';
+    countDisplay.style.left = x;
+  } else {
+    x = window.innerWidth - (window.scrollX + window.innerWidth - 10) + 'px';
+    countDisplay.style.left = '';
+    countDisplay.style.right = x;
+  }
+  const y = Math.min(clientRect.top + element.clientHeight + window.scrollY + 5, window.scrollY + window.innerHeight - 40) + 'px';
+  countDisplay.style.top = y;
 }
 
 /**
@@ -93,20 +100,20 @@ function setCoordinate(element) {
  * @param {Number} interval 間引きする間隔(デフォルトでは1280ms)
  * @returns {Function} 
  */
- var throttle = (function(callback, interval = 256) {
+var throttle = (function (callback, interval = 256) {
   var time = Date.now(),
-      lag,
-      debounceTimer,
-      debounceDelay = 16;
+    lag,
+    debounceTimer,
+    debounceDelay = 16;
 
-  return function(callback) {
+  return function (callback) {
     lag = time + interval - Date.now();
     if (lag < 0) {
       callback();
       time = Date.now();
     } else {
       clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(function() {
+      debounceTimer = setTimeout(function () {
         callback();
       }, (interval - lag + debounceDelay));
     }
