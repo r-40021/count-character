fe8514a34d1aab2d317965e5dd6ade795b67a39f
FrogApp <75155258+r-40021@users.noreply.github.com>
2024-02-24 15:06:02 +0900

    Reduce event

--- script/content.js
+++ script/content.js
@@ -1,7 +1,7 @@
 (() => {
   let elements = document.querySelectorAll('input:not([type]),input[type=text],input[type=search],input[type=tel],input[type=url],input[type=email],input[type=password],input[type=number],textarea');
   let countDisplay; // 文字数ディスプレイの要素
-  let opacityTimeout, displayTimeout; // 2秒間操作がなかったら透明度を下げる timeout と、 10秒間操作がなかったら非表示にする timeout
+  let opacityTimeout, displayTimeout; // 5秒間操作がなかったら透明度を下げる timeout と、 10秒間操作がなかったら非表示にする timeout
   let isRunningAwayFromCursor = false; // カーソルを避けているかどうか
 
   addEvent(false, elements);
@@ -9,16 +9,20 @@
   const observeElem = document.body;
   const observeConfig = {
     childList: true,
-    subtree: true
+    subtree: true,
+    characterData: false,
+    attributes: false,
   }
 
   /* 要素の変化を監視し、動的に生成された要素にもイベントを登録 */
   const mutationObserver = new MutationObserver(() => {
     mutationObserver.disconnect();
-    addEvent(true, elements);
     const newElements = document.querySelectorAll('input:not([type]),input[type=text],input[type=search],input[type=tel],input[type=url],input[type=email],input[type=password],input[type=number],textarea');
-    addEvent(false, newElements);
-    elements = newElements;
+    if (JSON.stringify(elements) !== JSON.stringify(newElements)) {
+      addEvent(true, elements);
+      addEvent(false, newElements);
+      elements = newElements;
+    }
     mutationObserver.observe(observeElem, observeConfig);
   });
 
@@ -30,28 +34,21 @@
    * @param {NodeList} elements テキストボックスの要素の NodeList
    */
   function addEvent(isRemoved, elements) {
-
     elements.forEach((element) => {
 
       const throttleSetCoordDinateToCurrentElement = () => throttle(setCoordDinateToCurrentElement);
 
       const setCoordDinateToCurrentElement = () => setCoordinate(element);
 
-      const handleCursorMove = (e) => mouseMove(e, element);
-
       const handleTextInput = () => {
-        const textLength = element.value.length;
         createDisplayElement(element);
-        countDisplay.textContent = textLength;
-        throttle(setCoordDinateToCurrentElement);
+        countDisplay.textContent = element.value.length;
       };
 
       const handleFocus = () => {
-        document.removeEventListener('mousemove', handleCursorMove);
         createDisplayElement(element);
         window.addEventListener('scroll', throttleSetCoordDinateToCurrentElement);
         window.addEventListener('resize', throttleSetCoordDinateToCurrentElement);
-        document.addEventListener('mousemove', handleCursorMove);
       };
 
       const handleBlur = () => {
@@ -59,7 +56,6 @@
         countDisplay = null;
         window.removeEventListener('scroll', throttleSetCoordDinateToCurrentElement);
         window.removeEventListener('resize', throttleSetCoordDinateToCurrentElement);
-        document.removeEventListener('mousemove', handleCursorMove);
       }
 
       if (isRemoved) {
@@ -75,9 +71,9 @@
         element.addEventListener('focus', handleFocus);
 
         element.addEventListener('blur', handleBlur);
-        
+
         // autofocus 要素への対応
-        if(document.activeElement === element) handleFocus();
+        if (document.activeElement === element) handleFocus();
       }
     })
   }
@@ -88,7 +84,7 @@
    * @param {Element} element 対象とするテキストボックスの要素 
    */
   function createDisplayElement(element) {
-    setOpacityTimeout(); // 2秒間操作がなかったら透明度を下げる
+    setOpacityTimeout(); // 5秒間操作がなかったら透明度を下げる
     if (!countDisplay) {
       // 文字数を表示する要素を生成
       countDisplay = document.createElement('div');
@@ -113,7 +109,7 @@
   }
 
   /**
-   * 文字数ディスプレイの透明度を2秒後に下げる
+   * 文字数ディスプレイの透明度を5秒後に下げる
    * @param {Boolean} [enableDisplay=true] 非表示のカウンターを再度表示するか否か
    */
   function setOpacityTimeout(enableDisplay = true) {
@@ -121,14 +117,14 @@
     clearTimeout(displayTimeout);
     if (countDisplay) {
       countDisplay.style.opacity = 0.9
-      if (enableDisplay )countDisplay.style.display = '';
+      if (enableDisplay) countDisplay.style.display = '';
     }
     opacityTimeout = setTimeout(() => {
       if (countDisplay) countDisplay.style.opacity = 0.2;
       displayTimeout = setTimeout(() => {
         if (countDisplay) countDisplay.style.display = 'none';
       }, 8000);
-    }, 2000);
+    }, 5000);
   }
 
 
@@ -142,14 +138,14 @@
     const clientRect = element.getBoundingClientRect();
     if (clientRect.right + window.scrollX + 5 + 10 * element.value.length.toString().length + 10 * 2 <= window.scrollX + window.innerWidth - 10) {
       countDisplay.style.right = '';
-      countDisplay.style.left =clientRect.right + window.scrollX + 5 + 'px';
+      countDisplay.style.left = clientRect.right + window.scrollX + 5 + 'px';
     } else {
       countDisplay.style.left = '';
       countDisplay.style.right = document.body.clientWidth - (window.scrollX + window.innerWidth - 10) + 'px';
     }
-    
-      countDisplay.style.top = Math.min(clientRect.top + element.clientHeight + window.scrollY + 5, window.scrollY + window.innerHeight - 50) + 'px';
-    
+
+    countDisplay.style.top = Math.min(clientRect.top + element.clientHeight + window.scrollY + 5, window.scrollY + window.innerHeight - 50) + 'px';
+
     isRunningAwayFromCursor = false;
   }
 
@@ -179,34 +175,4 @@
     }
   })();
 
-  /**
-   * マウスカーソルをよける
-   * @param {Object} e mousemoveイベントのオブジェクト
-   * @param {Object} element 対象となるテキストボックスのオブジェクト
-   */
-  function mouseMove(e, element) {
-    if(!countDisplay) return;
-
-    const clientRect = countDisplay.getBoundingClientRect();
-    const displayX = clientRect.left + window.scrollX + countDisplay.clientWidth / 2;
-    const displayY = clientRect.top + window.scrollY + countDisplay.clientHeight / 2;
-
-    const mouseX = e.pageX;
-    const mouseY = e.pageY;
-
-    if(Math.abs(mouseX - displayX) <= 15 + countDisplay.clientWidth && Math.abs(mouseY - displayY) <= 15 + countDisplay.clientHeight && countDisplay.style.display !== 'none') {
-      setOpacityTimeout(false); // 透明度をいったん戻す
-      const d = 80;
-      const angle = Math.atan2(displayY - mouseY, displayX - mouseX);
-      countDisplay.style.top = clientRect.top + window.scrollY + Math.sin(angle)*d + 'px';
-      isRunningAwayFromCursor = true;
-    } else if (isRunningAwayFromCursor && Math.abs(mouseX - displayX) > 15 + countDisplay.clientWidth && Math.abs(mouseY - displayY) > 15 + countDisplay.clientHeight){
-      setOpacityTimeout(false); // 透明度をいったん戻す
-      // カーソルが離れたら元の位置に戻す
-      setCoordinate(element, true);
-      isRunningAwayFromCursor = false;
-    }
-
-  }
-  
 })();
